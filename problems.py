c5bc83a19771d0c7dd8300f524ee426feba36162
justicecandelario <91089677+justicecandelario@users.noreply.github.com>
2021-12-02 17:40:28 -0500

    Update problems.py

--- problems.py
+++ problems.py
@@ -47,31 +47,64 @@ a, b, c = (3, 4, 8)
 var = None
 
 # Create a variable, lst, storing a list of the numbers 0-1000 (inclusive)
-lst = [1-1000]
+lst = []
+for i in range(0, 1001):
+  lst.append(i)
 
 # Print the sum and average of the list
+print(sum(lst))
+print(sum(lst)/len(lst))
 
 # Create a variable, ascii_low, storing a **set** of all lowercase ascii characters
+ascii_low = []
+for ch in string.ascii_lowercase:
+  ascii_low.append(ch)
+ascii_low = set(ascii_low)
 
 # Create a varaible, ascii_all, storing a **set** of all ascii characters
+ascii_all = []
+for ch in string.ascii_letters:
+  ascii_all.append(ch)
+ascii_all = set(ascii_all)
 
 # Create a variable, ascii_hi, storing the set different between ascii_all and
 # ascii_low, i.e. (ascii_all - ascii_low)
+ascii_hi = ascii_all - ascii_low
 
 # Create a dictionary with one key, your first name that maps to one value,
 # your last name
+namedictionary = {}
+namedictionary['Justice'] = 'Candelario'
 
 # Alter the dictionary to make your last name all caps
+namedictionary['Justice'] = namedictionary['Justice'].upper()
 
 # Here is a nested structure
 d = {'hello': [[{'cory': 'nezin'}, {'CORY': ['N', 'E', 'Z', 'I', 'N']}]]}
 # Change the 'Z' from the list ['N', 'E', 'Z', 'I', 'N'] inside to a 'z'
+((((d['hello'])[0])[1])['CORY'])[2] = ((((d['hello'])[0])[1])['CORY'])[2].lower()
 
 # Use a for loop to print numbers 1 through 10 (inclusive)
+for number in range(1, 11):
+  print(number)
 
 # Use a for loop to create a list of the first 10 cubes (0 through 9, cubed)
+cube = []
+for k in range(0, 10):
+  cube.append(k**3)
 
 # Now use a list comprehension to do the same
+j = [(n**3) for n in range(0, 10)]
 
 # Write a function 'unique' which takes a list and checks if each element is
 # unique, meaning there are no duplicates inside the list.
+def unique(my_list):
+  counts = {}
+  for element in my_list:
+    counts[element] = 0
+  for element in my_list:
+    counts[element] = counts[element]+1
+  for key in counts.key():
+    if counts[key]>1:
+      return False
+  return True
